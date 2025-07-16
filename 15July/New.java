b5af4409c2e961df8ae707628d74b525bc35615d
Praveen26-Dev <praveenpandey709@gmail.com>
2025-07-16 16:45:46 +0530

    first commit

---
+++ 15July/New.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class New{
+    public static void main(String a[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+    }
+}
+
+// how to get size direct in bytes 
+
+// contructor == to initialize the memory of object by their default values
