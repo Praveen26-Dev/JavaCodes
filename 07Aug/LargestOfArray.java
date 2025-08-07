f1ff9605d83ec9f39131e33d2c72032f93ecff69
Praveen26-Dev <praveenpandey709@gmail.com>
2025-08-07 18:14:02 +0530

    Today doing  some questions on  array

--- 07Aug/LargestOfArray.java
+++ 07Aug/LargestOfArray.java
@@ -3,12 +3,18 @@ public class LargestOfArray{
     public static void main(String args[]){
         int arr[]= {1,2,3,4,3,2,1};
         int largest =Integer.MIN_VALUE;
-        int i=0;
-            while(i<arr.length-1){
-                if(arr[i]>=arr[i++]){
-                    largest=arr[i];
-                }
-            
+        
+        int first=0;
+        int second= 1;
+            while(first<arr.length-1){
+                if(arr[first]>largest){
+                    largest=arr[first];
+                } 
+                if(arr[second]>largest){
+                    largest=arr[first];
+                }    
+            first++;
+            second++;
       }
       System.out.println(largest);
     }
