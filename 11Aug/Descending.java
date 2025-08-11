78f1c10a033aa691a51632238640497492955e85
Praveen26-Dev <praveenpandey709@gmail.com>
2025-08-11 17:30:59 +0530

    Today doing practise questions of largest,descending,Nth Largest

---
+++ 11Aug/Descending.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Descending{
+    public static void main(String args[]){
+       int arr[] = {1,3,6,2,4,5,8,9};
+       int n=3;
+       for(int i=0;i<arr.length-1;i++){
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[i]<arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+       }
+        System.out.print(arr[n-1]+" ");
+    }
+}
