1bd580e27007bee7895ee7cbeb8fd5ad4da949ac
Praveen26-Dev <praveenpandey709@gmail.com>
2025-08-20 13:37:13 +0530

    Today doing practise questions of advanced questions like target sum,remove first occurance and all occurance

--- 14Aug/H1.java
+++ 14Aug/H1.java
@@ -1,20 +1,26 @@
-class H1{
-public static void main(String[]arg){
+public class H1 {
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 3, 4, 1, 2, 3, 0, 2, 6, 0, 7, 2, 9};
 
-int arr[] = {8,2, 1,8, 3, 1, 2, 2, 9, 7, 3, 4, 2};
-for (int i = 0; i < arr.length; i++) {
-    for (int j = i + 1; j < arr.length; j++) {
-        if (arr[i] == arr[j] && arr[i] != -1) {
-            arr[i] = -1; 
-            break;       
+        // loop for each element
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) continue;
+
+            // check if element repeats later
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arr[i] = -1;  // remove ONLY the first occurrence
+                    break;        // stop after removing the first one
+                }
+            }
+        }
+
+        // print result
+        System.out.print("Result: ");
+        for (int num : arr) {
+            if (num != -1) {
+                System.out.print(num + " ");
+            }
         }
     }
-}
-
-for (int i = 0; i < arr.length; i++) {
-    if (arr[i] != -1) {
-        System.out.print(arr[i] + " ");
-    }
-}
-}
 }
--- 14Aug/H2.java
+++ 14Aug/H2.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class H2{
     public static void main(String args[]){
         int arr[] = {1,2,3,3,4,1,2,3,0,2,3,6,0,7,2,9};
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
                 if(arr[i]==arr[j]){
                     arr[i]=-1;
