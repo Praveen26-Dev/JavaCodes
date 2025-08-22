6673ffbb860f40abaddd4385728d99adfdb8b81e
Praveen26-Dev <praveenpandey709@gmail.com>
2025-08-22 15:42:54 +0530

    Today doing practise question

--- 25July/Factorial.java
+++ 25July/Factorial.java
@@ -1,10 +1,16 @@
-int fact=1;
-    cin>>num;
+import java.util.*;
+public class Factorial{
+ public static void main(String args[]){
+    int fact=1;
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
     if(num<=1){
-        cout<<"factorial is "<<num<<endl;
+        System.out.println("factorial is "+num);
     }
 
     for(int i=1;i<=num;i++){
         fact = fact*i;
     }
-    cout<<fact;
+    System.out.println(fact);
+} 
+}   
