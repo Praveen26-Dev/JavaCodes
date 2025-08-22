b36eb651a41e7cfb0a4e9b73d6fc20b8cc1b40ee
Praveen26-Dev <praveenpandey709@gmail.com>
2025-08-22 16:03:08 +0530

    Today doing practise question

--- 25July/HCF.java
+++ 25July/HCF.java
@@ -9,19 +9,11 @@ public class HCF {
 
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
-
-        // Euclidean algorithm
-        int hcf = findHCF(a, b);
-
-        System.out.println("HCF of " + a + " and " + b + " is: " + hcf);
-    }
-
-    public static int findHCF(int a, int b) {
-        while (b != 0) {
+         while (b != 0) {
             int temp = b;
             b = a % b;
             a = temp;
         }
-        return a;
+        System.out.println("HCF of two number is = "+a);
     }
 }
