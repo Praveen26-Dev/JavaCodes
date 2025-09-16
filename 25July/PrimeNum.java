2d065ad576151538a4297be7c53704597465753c
Praveen26-Dev <praveenpandey709@gmail.com>
2025-09-16 16:54:13 +0530

    some changes in previos codes

--- 25July/PrimeNum.java
+++ 25July/PrimeNum.java
@@ -1,11 +1,11 @@
 public class PrimeNum{
     public static void main(String args[]){
-        int no=20;
+        int no=17;
         int count=0;
         for(int i=1;i<=20;++i){
             if(no%i==0){
                 ++count;
-                // System.out.println(i);
+                System.out.println(i); 
             }
         }
         if(count==2){
--- 30July/Armstrong.java
+++ 30July/Armstrong.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class Armstrong{
     public static void main(String args[]){
-        int n=153;
+        int n=151;
         int no = n;
         int sum =0;
         while(n!=0)
         {
             int digit = n%10;
-            sum = digit*digit*digit;
+            sum = sum+digit*digit*digit;
             n = n/10;
         }
         if(sum==no){
